2d80f65bfed91ed341897993e6d888d3038bfd2a
Felix Grant <fgrant505@outlook.com>
2023-09-21 14:11:24 +0100

    Extracts all variable names to an array

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,39 @@
 import * as vscode from 'vscode';
 
-function extractVariableList(symbols: vscode.DocumentSymbol[]): vscode.DocumentSymbol[] {
-	const variables = symbols.filter(symbol => symbol.kind === vscode.SymbolKind.Constant || symbol.kind === vscode.SymbolKind.Variable);
+function extractVariableNames(symbols: vscode.DocumentSymbol[]): vscode.DocumentSymbol[] {
+	const variables = symbols.filter(symbol =>
+		symbol.kind === vscode.SymbolKind.Constant ||
+		symbol.kind === vscode.SymbolKind.Variable);
 						
-	return variables.concat(symbols.map(symbol => extractVariableList(symbol.children)).reduce((a, b) => a.concat(b), []));
+	return variables.concat(symbols.map(symbol =>
+		extractVariableNames(symbol.children)).reduce((a, b) => a.concat(b), []));
+}
+
+async function runVariableCheck() {
+	const textEditor = vscode.window.activeTextEditor;
+	let variables: string[] = [];
+	// Text editor can be undefined, so check
+	if (!textEditor) {
+		console.log("Failed to access text editor");
+		return;
+	}
+
+	const symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>
+	('vscode.executeDocumentSymbolProvider', textEditor.document.uri);
+
+	if (symbols !== undefined) {
+		for (const variable of extractVariableNames(symbols)) {
+			variables.push(variable.name);
+		}
+	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const runVariableCheck = vscode.commands.registerCommand('variable-name-checker.run-variable-check', () => {
-
-		const textEditor = vscode.window.activeTextEditor;
-		let variables: string[] = [];
-
-		// Text editor can be undefined, so check
-		if (!textEditor) {
-			console.log("Failed to access text editor");
-			return;
-		}
-
-		// Create a list of all variables and constants defined in the text editor
-		vscode.commands.executeCommand<vscode.DocumentSymbol[]>
-			('vscode.executeDocumentSymbolProvider', textEditor.document.uri)
-			.then(symbol => {
-				if (!(symbol === undefined)) {
-					for (const variable of extractVariableList(symbol)) {
-						variables.push(variable.name);
-					}
-				}
-			});
-
+	const variableCheck = vscode.commands.registerCommand('variable-name-checker.run-variable-check', () => {
+		runVariableCheck();
 	});
-
-	context.subscriptions.push(runVariableCheck);
+	context.subscriptions.push(variableCheck);
 }
 
 export function deactivate() {}
